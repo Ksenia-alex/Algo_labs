e5481c23cde6ac8b6148c06e4cde67b36184bc4d
kseniatolstuhina <kt26112006@mail.ru>
2024-10-13 22:20:51 +0300

    corr Task7

--- lab2/Task7/task7.py
+++ lab2/Task7/task7.py
@@ -10,3 +10,9 @@ def find_max(a):
         if summ < 0:
             summ = 0
     return a[start_indx:end_indx + 1]
+
+
+with open('output.txt', 'w') as file:
+    f_input = list(map(int, open('input.txt').readline().split()))
+    a = find_max(f_input)
+    file.write(' '.join(map(str, a)))
