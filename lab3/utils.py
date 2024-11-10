4a125c3f607609c03899975abca1defe516dea8f
kseniatolstuhina <kt26112006@mail.ru>
2024-11-10 03:12:05 +0300

    corr utils.py

--- lab3/utils.py
+++ lab3/utils.py
@@ -9,10 +9,14 @@ def generation():
         file.write(' '.join(map(str, random.sample(range(10 ** 9), n))))
 
 
-def generations_str():
+def generations_str(number):
     with open('input.txt', 'w') as file:
         n = random.randint(1, 10 ** 5 + 1)
-        file.write(str(n) + '\n')
+        if number == '8':
+            k = random.randint(1, n)
+            file.write(str(n) + ' ' + str(k) + '\n')
+        if number == '9':
+            file.write(str(n) + '\n')
         for i in range(n):
             file.write(str(random.randint(-10 ** 9, 10 ** 9)) + ' ' + str(random.randint(-10 ** 9, 10 ** 9)) + '\n')
 
@@ -23,6 +27,12 @@ def read():
     n, data = file_input.readline(), list(map(int, file_input.readline().split()))
     return n, data
 
+
+def read_coor():
+    file = open('input.txt')
+    n = file.readline()
+    coor = [(int(i.split()[0]), int(i.split()[1])) for i in file.readlines()]
+    return n, coor
 
 def write(res):
     """Функция записи данных в файл"""
