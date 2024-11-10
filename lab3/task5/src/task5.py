cc04ba4bd03e652a706751024558f5a4469d2a9a
kseniatolstuhina <kt26112006@mail.ru>
2024-11-10 13:29:30 +0300

    corr task5 and test2

--- lab3/task5/src/task5.py
+++ lab3/task5/src/task5.py
@@ -1,3 +1,7 @@
 def find_index_h(arr: [int]) -> int:
     """"Функция для нахождения индекса Хирша"""
-    return sorted(arr)[::-1][round(len(arr) / 2)]
+    arr.sort(reverse=True)
+    for i, count_citation in enumerate(arr, 1):
+        if count_citation < i:
+            return i - 1
+    return len(arr)
--- lab3/task5/tests/test2.py
+++ lab3/task5/tests/test2.py
@@ -18,10 +18,10 @@ class MinDistTestCase(unittest.TestCase):
     def test_correctness(self):
         self.assertEqual(find_index_h([3, 0, 6, 1, 5]), 3)
         self.assertEqual(find_index_h([1, 3, 1]), 1)
-        self.assertEqual(find_index_h([1, 5, 7, 2, 8, 0, 2, 5, 2, 4]), 2)
+        self.assertEqual(find_index_h([1, 5, 7, 2, 8, 0, 2, 5, 2, 4]), 4)
 
 
     def test_time_memory(self):
         self.output_design(1, find_index_h, 3, [3, 0, 6, 1, 5])
         self.output_design(2, find_index_h, 1, [1, 3, 1])
-        self.output_design(3, find_index_h, 2, [1, 5, 7, 2, 8, 0, 2, 5, 2, 4])
+        self.output_design(3, find_index_h, 4, [1, 5, 7, 2, 8, 0, 2, 5, 2, 4])
