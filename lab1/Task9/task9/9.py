794cbffac9ba342c8e83be7843b613ee77e07137
kseniatolstuhina <kt26112006@mail.ru>
2024-12-11 02:57:53 +0300

    add

--- lab1/Task9/task9/9.py
+++ lab1/Task9/task9/9.py
@@ -5,10 +5,10 @@ import random
 start = time.perf_counter()
 tracemalloc.start()
 
-file = open('input.txt')
+file = open('../input.txt')
 a, b = file.readline().split()
 
-with open('output.txt', 'w') as file:
+with open('../output.txt', 'w') as file:
     file.write(bin(int(a, 2) + int(b, 2))[2:])
 
 print(time.perf_counter() - start)
